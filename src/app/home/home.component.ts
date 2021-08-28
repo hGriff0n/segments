ac0d6f61dfd9d069bcdeaeaa3034e6eff0db97c5
hGriff0n <ghooper96@gmail.com>
2021-08-28 13:28:23 -0400

    Focus Map on Users Current Location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,11 +7,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  location: [number, number] = [0, 0];
+  zoom: [number] = [9];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    console.log('HomeComponent INIT');
+    this.getLocation();
+  }
+
+  getLocation(): void{
+    // Mapbox Geolocate Control?
+    if (navigator.geolocation) {
+      // Possible to do before load?
+        navigator.geolocation.getCurrentPosition((position)=>{
+          this.location = [position.coords.longitude, position.coords.latitude];
+          this.zoom = [12];
+        });
+    } else {
+       console.log("No support for geolocation")
+    }
   }
 
   onLoad(event) {
