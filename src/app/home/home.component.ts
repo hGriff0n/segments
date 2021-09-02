1d34904b553779dd802f9ec4d73d7a022e43a3d9
hGriff0n <ghooper96@gmail.com>
2021-09-01 20:24:59 -0400

    Add Basic Popup on Hover With Segment Information

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Map } from 'mapbox-gl';
+import { Map, Popup } from 'mapbox-gl';
 import * as polyline from '@mapbox/polyline';
 import { Strava, SummarySegment, DetailedSegment } from 'strava';
 
@@ -14,6 +14,7 @@ export class HomeComponent implements OnInit {
   zoom: [number] = [9];
   map: Map;
   strava: Strava;
+  popup: Popup;
 
   constructor(private router: Router) { }
 
@@ -24,11 +25,13 @@ export class HomeComponent implements OnInit {
       client_secret: 'e27db3aebb572370faf8bdc3acc1e0aae016d622',
       refresh_token: '7511a603ab56d2772550c0b11f51a80e3dc3ab33 ',
     });
+    this.popup = new Popup({closeButton: false, closeOnClick: true, closeOnMove: true });
   }
 
   onLoad(event: Map) {
     this.map = event;
     this.map.resize();
+    this.map.getCanvas().style.cursor = '';
 
     // Load all of the starred segments and render them on the map
     // Use `Promise.all` to parallelize the network calls
@@ -36,7 +39,7 @@ export class HomeComponent implements OnInit {
       var segments = [];
       for (var starred of await this.starredSegments()) {
         segments.push(this.getSegment(starred.id).then(segment => {
-          this.paintSegment("" + segment.id, segment.map.polyline);
+          this.paintSegment(segment);
         }));
       }
 
@@ -53,25 +56,48 @@ export class HomeComponent implements OnInit {
     // this.map.on('click', 'markers', e => this.displayDetailedPopup(e));
   }
 
+  // TODO: Could also put all segment jsons into 1 layer, would that help performance?
+  // Can I modify a layer to add in items afterwards?
   // Would be potentially beneficial to use mgl-layer and mgl-geojson-source
   // but that might make some things more difficult so leaving as-is for now
-  private paintSegment(id: string, segment_polyline: string) {
-    const line: GeoJSON.LineString = polyline.toGeoJSON(segment_polyline);
+  private paintSegment(segment: DetailedSegment) {
+    var id = "" + segment.id
+
+    const line: GeoJSON.LineString = polyline.toGeoJSON(segment.map.polyline);
     this.map.addSource(id, { type: 'geojson', data: { type: 'Feature', properties: {}, geometry: line } });
     this.map.addLayer({
       id: id,
       type: 'line',
       source: id,
       layout: { 'line-join': 'round', 'line-cap': 'round' },
-      paint: { 'line-color': '#EE4B2B', 'line-width': [
-        'interpolate',
-        ['exponential', 2],
-        ['zoom'],
-        0, ["*", 12, ["^", 2, -6]],
-        24, ["*", 12, ["^", 2, 8]]
-    ],
- }
+      paint: {
+        'line-color': '#EE4B2B', 'line-width': [
+          'interpolate',
+          ['exponential', 2],
+          ['zoom'],
+          0, ["*", 12, ["^", 2, -6]],
+          24, ["*", 12, ["^", 2, 8]]
+        ],
+      }
     });
+
+    this.map.on('mouseenter', id, (e) => {
+      this.map.getCanvas().style.cursor = 'pointer';
+      this.popup.setLngLat(e.lngLat).setHTML(this.formatPopup(segment)).addTo(this.map);
+      console.log("Scrolled over " + id);
+      console.log(e)
+    });
+    this.map.on('mouseleave', id, (e) => {
+      this.map.getCanvas().style.cursor = '';
+      this.popup.remove();
+    });
+  }
+
+  // TODO: Format popup with useful information
+  // TODO: Find a way to make popup "stickier"
+  // TODO: Move popup to own component class (ie. automate formatting)
+  private formatPopup(segment: DetailedSegment): string {
+    return segment.name + " (" + segment.average_grade + "%)";
   }
 
   private async starredSegments(): Promise<SummarySegment[]> {
@@ -79,23 +105,23 @@ export class HomeComponent implements OnInit {
   }
 
   private async getSegment(id: number): Promise<DetailedSegment> {
-    return this.strava.segments.getSegmentById({id: id});
+    return this.strava.segments.getSegmentById({ id: id });
   }
 
-  private displayMiniPopup(e) {}
-  private closeMiniPopup(e) {}
-  private displayDetailedPopup(e) {}
+  private displayMiniPopup(e) { }
+  private closeMiniPopup(e) { }
+  private displayDetailedPopup(e) { }
 
-  private getLocation(): void{
+  private getLocation(): void {
     // Mapbox Geolocate Control?
     if (navigator.geolocation) {
       // Possible to do before load?
-        navigator.geolocation.getCurrentPosition((position)=>{
-          this.location = [position.coords.longitude, position.coords.latitude];
-          this.zoom = [12];
-        });
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.location = [position.coords.longitude, position.coords.latitude];
+        this.zoom = [12];
+      });
     } else {
-       console.log("No support for geolocation")
+      console.log("No support for geolocation")
     }
   }
 }
